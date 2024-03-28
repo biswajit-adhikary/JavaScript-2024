592be34b2afaffe5217aca2efb58c305d08d90ab
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2024-03-28 22:33:40 +0600

    let, const and var

--- 28-03-2024/script.js
+++ 28-03-2024/script.js
@@ -27,23 +27,39 @@
 // console.log(continent);
 // console.log(population);
 
-// Section 2: 11. Data Types
-// JavaScript Data Types:
-// 1. String
-// 2. Number
-// 3. Boolean
-// 4. Undefined
-// 5. Null
-// 6. Symbol
-// 7. Object
-// 8. Bigint
-let firstName = "Adrita"; // 1. String
-console.log(typeof firstName);
-let age = 7; // 2. Number
-console.log(typeof age);
-let startSchool = true; // 3. Boolean
-console.log(typeof startSchool);
-let futurePlan; // 4. Undefined
-console.log(typeof futurePlan);
-let nullData = null; // 5. Null
-console.log(typeof null);
+// // Section 2: 11. Data Types
+// // JavaScript Data Types:
+// // 1. String
+// // 2. Number
+// // 3. Boolean
+// // 4. Undefined
+// // 5. Null
+// // 6. Symbol
+// // 7. Object
+// // 8. Bigint
+// let firstName = "Adrita"; // 1. String
+// console.log(typeof firstName);
+// let age = 7; // 2. Number
+// console.log(typeof age);
+// let startSchool = true; // 3. Boolean
+// console.log(typeof startSchool);
+// let futurePlan; // 4. Undefined
+// console.log(typeof futurePlan);
+// let nullData = null; // 5. Null
+// console.log(typeof null);
+
+// Section 2: 12. let, const and var
+let age = 6;
+console.log(age);
+age = 7; // Change value possible in let
+console.log(age);
+
+const birthYear = 2017;
+console.log(birthYear);
+// birthYear = 2016; // Change value not possible in const
+// const education; // Reassign value in const isn't possible
+
+var myJob = "Programmer";
+console.log(myJob);
+myJob = "Teacher";
+console.log(myJob);
