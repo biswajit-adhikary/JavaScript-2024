ad12bfa19637d3619afd7c8769f34b8552058805
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2024-03-28 22:35:00 +0600

    28-03-24 All Code

--- 28-03-2024/script.js
+++ 28-03-2024/script.js
@@ -1,52 +1,52 @@
-// // Section 2: 8. Linking a JavaScript File
-// let js = "best";
-// if (js === "best") alert("JavaScript is best!");
-// console.log(40 + 8 + 23 - 10);
+// Section 2: 8. Linking a JavaScript File
+let js = "best";
+if (js === "best") alert("JavaScript is best!");
+console.log(40 + 8 + 23 - 10);
 
-// // Section 2: 9. Values and Variables
-// console.log("Adrita");
-// console.log(7);
-// let firstName = "Adrita";
-// console.log(firstName);
-// console.log(firstName);
+// Section 2: 9. Values and Variables
+console.log("Adrita");
+console.log(7);
+let firstName = "Adrita";
+console.log(firstName);
+console.log(firstName);
 
-// // Variables writing rules
-// let fullName = "Adrita Adhikary";
-// let $age = 7;
-// let _school = "Primary School";
-// let PI = 3.1415;
-// let myFristJob = "Customer Care Representative";
-// let myCurrentJob = "Web Develoer";
-// console.log(myCurrentJob);
+// Variables writing rules
+let fullName = "Adrita Adhikary";
+let $age = 7;
+let _school = "Primary School";
+let PI = 3.1415;
+let myFristJob = "Customer Care Representative";
+let myCurrentJob = "Web Develoer";
+console.log(myCurrentJob);
 
-// // Section 2: 10. Practice Assignments
-// let country = "Bangladesh";
-// let continent = "South Asia";
-// let population = "174.70 million";
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+// Section 2: 10. Practice Assignments
+let country = "Bangladesh";
+let continent = "South Asia";
+let population = "174.70 million";
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// // Section 2: 11. Data Types
-// // JavaScript Data Types:
-// // 1. String
-// // 2. Number
-// // 3. Boolean
-// // 4. Undefined
-// // 5. Null
-// // 6. Symbol
-// // 7. Object
-// // 8. Bigint
-// let firstName = "Adrita"; // 1. String
-// console.log(typeof firstName);
-// let age = 7; // 2. Number
-// console.log(typeof age);
-// let startSchool = true; // 3. Boolean
-// console.log(typeof startSchool);
-// let futurePlan; // 4. Undefined
-// console.log(typeof futurePlan);
-// let nullData = null; // 5. Null
-// console.log(typeof null);
+// Section 2: 11. Data Types
+// JavaScript Data Types:
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. Object
+// 8. Bigint
+let firstName = "Adrita"; // 1. String
+console.log(typeof firstName);
+let age = 7; // 2. Number
+console.log(typeof age);
+let startSchool = true; // 3. Boolean
+console.log(typeof startSchool);
+let futurePlan; // 4. Undefined
+console.log(typeof futurePlan);
+let nullData = null; // 5. Null
+console.log(typeof null);
 
 // Section 2: 12. let, const and var
 let age = 6;
