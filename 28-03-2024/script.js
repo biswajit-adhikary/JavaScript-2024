79d092520f05b5894d531a5de97a6392854073ae
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2024-03-28 21:01:22 +0600

    Values and Variables Practice Assignments

--- 28-03-2024/script.js
+++ 28-03-2024/script.js
@@ -3,18 +3,26 @@
 // if (js === "best") alert("JavaScript is best!");
 // console.log(40 + 8 + 23 - 10);
 
-// Section 2: 9. Values and Variables
-console.log("Adrita");
-console.log(7);
-let firstName = "Adrita";
-console.log(firstName);
-console.log(firstName);
+// // Section 2: 9. Values and Variables
+// console.log("Adrita");
+// console.log(7);
+// let firstName = "Adrita";
+// console.log(firstName);
+// console.log(firstName);
 
-// Variables writing rules
-let fullName = "Adrita Adhikary";
-let $age = 7;
-let _school = "Primary School";
-let PI = 3.1415;
-let myFristJob = "Customer Care Representative";
-let myCurrentJob = "Web Develoer";
-console.log(myCurrentJob);
+// // Variables writing rules
+// let fullName = "Adrita Adhikary";
+// let $age = 7;
+// let _school = "Primary School";
+// let PI = 3.1415;
+// let myFristJob = "Customer Care Representative";
+// let myCurrentJob = "Web Develoer";
+// console.log(myCurrentJob);
+
+// Section 2: 10. Practice Assignments
+let country = "Bangladesh";
+let continent = "South Asia";
+let population = "174.70 million";
+console.log(country);
+console.log(continent);
+console.log(population);
