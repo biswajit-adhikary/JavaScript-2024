a0cb814aff60040a9f367b3603667520e5697b24
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2024-03-28 20:38:48 +0600

    Values and Variables

--- 28-03-2024/script.js
+++ 28-03-2024/script.js
@@ -1,4 +1,20 @@
-// Linking a JavaScript File
-let js = "best";
-if (js === "best") alert("JavaScript is best!");
-console.log(40 + 8 + 23 - 10);
+// // Section 2: 8. Linking a JavaScript File
+// let js = "best";
+// if (js === "best") alert("JavaScript is best!");
+// console.log(40 + 8 + 23 - 10);
+
+// Section 2: 9. Values and Variables
+console.log("Adrita");
+console.log(7);
+let firstName = "Adrita";
+console.log(firstName);
+console.log(firstName);
+
+// Variables writing rules
+let fullName = "Adrita Adhikary";
+let $age = 7;
+let _school = "Primary School";
+let PI = 3.1415;
+let myFristJob = "Customer Care Representative";
+let myCurrentJob = "Web Develoer";
+console.log(myCurrentJob);
